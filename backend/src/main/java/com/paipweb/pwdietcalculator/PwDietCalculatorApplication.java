3e5ed7857276970a602f06dd005b5abc3a53632c
RubyMyDear <kmlkamilek@gmail.com>
2019-09-12 21:02:15 +0200

    Backend initial project

---
+++ backend/src/main/java/com/paipweb/pwdietcalculator/PwDietCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.paipweb.pwdietcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwDietCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwDietCalculatorApplication.class, args);
+	}
+
+}
